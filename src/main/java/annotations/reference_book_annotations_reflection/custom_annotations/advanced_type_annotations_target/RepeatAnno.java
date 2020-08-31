65723dac2e00257fde8f1bd86d1b07549ee26fe4
sousajo <josesousa@rentalcars.com>
2020-08-31 23:42:21 +0100

    type annotations getAnnotationByType

--- src/main/java/annotations/reference_book_annotations_reflection/custom_annotations/advanced_type_annotations_target/RepeatAnno.java
+++ src/main/java/annotations/reference_book_annotations_reflection/custom_annotations/advanced_type_annotations_target/RepeatAnno.java
@@ -2,6 +2,7 @@ package annotations.reference_book_annotations_reflection.custom_annotations.adv
 
 import java.lang.annotation.*;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 
 /**
  * Created by sousaJ on 31/08/2020
@@ -25,8 +26,8 @@ import java.lang.reflect.Method;
 public class RepeatAnno {
 
     // Repeat Myanno in myMeth
-    @MyAnno(str = "First Annotation", val = 1)
-    @MyAnno(str = "Second Annotation", val = 2)
+    @MyAnno(str = "First Annotation", val = -1)
+    @MyAnno(str = "Second Annotation", val = 100)
     public static void myMeth(String str, int i){
         RepeatAnno obj = new RepeatAnno();
         try{
@@ -35,9 +36,16 @@ public class RepeatAnno {
             // Obtain the annotations for myMeth
             Method method = cl.getMethod("myMeth", String.class, int.class);
 
-            // display the repeated MyAnno annotations
+            // display the repeated MyAnno annotations, note that the container
+            // annotation is passed as the type to get
             Annotation anno = method.getAnnotation(MyRepeatableAnnos.class);
             System.out.println("anno = " + anno);
+
+            // another way of getting all annotations
+            // note that here is the annotation itself
+            Annotation[] annos = method.getAnnotationsByType(MyAnno.class);
+            Arrays.stream(annos)
+                    .forEach(System.out::println);
         }catch (NoSuchMethodException ex){
             System.out.println("Method not found");
         }
